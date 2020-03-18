e80cdb06dc7758e491a2986dfaaf5ee19e70953d
rjuyal123 <rajitjuyal@gmail.com>
2020-03-18 12:34:31 +0530

    Update background.js
    Added more condition to meeting notification.

--- notifications/background.js
+++ notifications/background.js
@@ -1,7 +1,12 @@
 var title = "";
 let folders = [];
 var myMap = new Map();
-let stop = true;
+let stopNow = false;
+let stop15 = false;
+let stop10 = false;
+let stop5 = false;
+let stopcust = false;
+
 
 function getUnreadCount(current, folderName) {
   chrome.tabs.executeScript(current.id, {
@@ -34,42 +39,57 @@ function getUpComingMeeting(current) {
 
 
     console.log(result[0].includes(" 2 min"));
-    if (result != "" && result[0].includes("Now") && stop) {
+    if (result != "" && result[0].includes("Now") && !stopNow) {
       new Notification(`Reminder`, {
         icon: '48.png',
-        body: `${result[0]}`
+        body: `${result[0]}`,
+        requireInteraction: true
       });
-      stop = false;
+      stopNow = true;
+      stop15 = false;
+      stop10 = false;
+      stop5 = false;
+      stopcust = false;
     }
 
-    if (result != "" && JSON.parse(localStorage.min5) && result[0].includes(" 5 min")) {
+    if (result != "" && JSON.parse(localStorage.min5) && result[0].includes(" 5 min") && !stop5) {
       new Notification(`Reminder`, {
         icon: '48.png',
         body: `${result[0]}`
       });
+      stopNow = false;
+
+      stop5 = true;
     }
 
-    if (result != "" && JSON.parse(localStorage.min10) && result[0].includes(" 10 min")) {
+    if (result != "" && JSON.parse(localStorage.min10) && result[0].includes(" 10 min") && !stop10) {
       new Notification(`Reminder`, {
         icon: '48.png',
         body: `${result[0]}`
       });
+      stopNow = false;
+
+      stop10 = true;
     }
 
-    if (result != "" && JSON.parse(localStorage.min15) && result[0].includes("15 min")) {
+    if (result != "" && JSON.parse(localStorage.min15) && result[0].includes("15 min") && !stop15) {
       new Notification(`Reminder`, {
         icon: '48.png',
         body: `${result[0]}`
       });
-      stop = true;
+      stopNow = false;
+
+      stop15 = true;
     }
 
-    if (result != "" && JSON.parse(localStorage.iscust) && result[0].includes(" " + localStorage.custmin + " min")) {
+    if (result != "" && JSON.parse(localStorage.iscust) && result[0].includes(" " + localStorage.custmin + " min") && !stopcust) {
       new Notification(`Reminder`, {
         icon: '48.png',
         body: `${result[0]}`
       });
-      stop = true;
+      stopcust = true;
+      stopNow = false;
+
     }
   });
 
