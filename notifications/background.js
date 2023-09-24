a7074b146c8b366108a64899c48caafc0876227c
Rajit Juyal <rajitjuyal@live.in>
2023-09-24 11:10:55 +0530

    Update the query and added a new icons

--- notifications/background.js
+++ notifications/background.js
@@ -7,121 +7,146 @@ let stop10 = false;
 let stop5 = false;
 let stopcust = false;
 
-
 function getUnreadCount(current, folderName) {
-  chrome.tabs.executeScript(current.id, {
-    code: 'document.querySelectorAll("[title=\'' + folderName + '\'] > span:nth-child(3)>span:first-child")[0].innerText'
-  }, function (result) {
-    if (result[0] !== null) {
-      // console.log(result[0]);
-      let msg = result[0].split('\n')[0];
-      if (myMap.has(folderName) && myMap.get(folderName) < parseInt(msg)) {
-        myMap.set(folderName, parseInt(msg));
-        new Notification(`${folderName} has ${msg} unread mails`, {
-          icon: '48.png',
-          body: 'Check your mail.'
-        });
+  chrome.tabs.executeScript(
+    current.id,
+    {
+      code:
+        "document.querySelectorAll(\"[title^='" +
+        folderName +
+        "'] > span:nth-child(3)>span:first-child>span:first-child\")[0].innerText",
+    },
+    function (result) {
+      if (result[0] !== null) {
+        // console.log(result[0]);
+        let msg = result[0].split("\n")[0];
+        if (myMap.has(folderName) && myMap.get(folderName) < parseInt(msg)) {
+          myMap.set(folderName, parseInt(msg));
+          new Notification(`${folderName} has ${msg} unread mails`, {
+            icon: "toast.png",
+            body: "Check your mail.",
+          });
+        } else {
+          myMap.set(folderName, parseInt(msg));
+        }
       } else {
-        myMap.set(folderName, parseInt(msg));
+        myMap.set(folderName, 0);
+        console.log(myMap);
       }
-    } else {
-      myMap.set(folderName, 0);
-      console.log(myMap);
     }
-  });
-
+  );
 }
 
 function getUpComingMeeting(current) {
-  chrome.tabs.executeScript(current.id, {
-    code: 'document.querySelector("[data-automation-id=\'UpNext\']>span>div>div>div").innerText'
-  }, function (result) {
+  chrome.tabs.executeScript(
+    current.id,
+    {
+      code: "document.querySelector(\"[data-automation-id='UpNext']>span>div>div>div\").innerText",
+    },
+    function (result) {
+      console.log(result[0].includes(" 2 min"));
+      if (result != "" && result[0].includes("Now") && !stopNow) {
+        new Notification(`Reminder`, {
+          icon: "48.png",
+          body: `${result[0]}`,
+          requireInteraction: true,
+        });
+        stopNow = true;
+        stop15 = false;
+        stop10 = false;
+        stop5 = false;
+        stopcust = false;
+      }
 
+      if (
+        result != "" &&
+        JSON.parse(localStorage.min5) &&
+        result[0].includes(" 5 min") &&
+        !stop5
+      ) {
+        new Notification(`Reminder`, {
+          icon: "48.png",
+          body: `${result[0]}`,
+        });
+        stopNow = false;
 
-    console.log(result[0].includes(" 2 min"));
-    if (result != "" && result[0].includes("Now") && !stopNow) {
-      new Notification(`Reminder`, {
-        icon: '48.png',
-        body: `${result[0]}`,
-        requireInteraction: true
-      });
-      stopNow = true;
-      stop15 = false;
-      stop10 = false;
-      stop5 = false;
-      stopcust = false;
+        stop5 = true;
+      }
+
+      if (
+        result != "" &&
+        JSON.parse(localStorage.min10) &&
+        result[0].includes(" 10 min") &&
+        !stop10
+      ) {
+        new Notification(`Reminder`, {
+          icon: "48.png",
+          body: `${result[0]}`,
+        });
+        stopNow = false;
+
+        stop10 = true;
+      }
+
+      if (
+        result != "" &&
+        JSON.parse(localStorage.min15) &&
+        result[0].includes("15 min") &&
+        !stop15
+      ) {
+        new Notification(`Reminder`, {
+          icon: "48.png",
+          body: `${result[0]}`,
+        });
+        stopNow = false;
+
+        stop15 = true;
+      }
+
+      if (
+        result != "" &&
+        JSON.parse(localStorage.iscust) &&
+        result[0].includes(" " + localStorage.custmin + " min") &&
+        !stopcust
+      ) {
+        new Notification(`Reminder`, {
+          icon: "48.png",
+          body: `${result[0]}`,
+        });
+        stopcust = true;
+        stopNow = false;
+      }
     }
-
-    if (result != "" && JSON.parse(localStorage.min5) && result[0].includes(" 5 min") && !stop5) {
-      new Notification(`Reminder`, {
-        icon: '48.png',
-        body: `${result[0]}`
-      });
-      stopNow = false;
-
-      stop5 = true;
-    }
-
-    if (result != "" && JSON.parse(localStorage.min10) && result[0].includes(" 10 min") && !stop10) {
-      new Notification(`Reminder`, {
-        icon: '48.png',
-        body: `${result[0]}`
-      });
-      stopNow = false;
-
-      stop10 = true;
-    }
-
-    if (result != "" && JSON.parse(localStorage.min15) && result[0].includes("15 min") && !stop15) {
-      new Notification(`Reminder`, {
-        icon: '48.png',
-        body: `${result[0]}`
-      });
-      stopNow = false;
-
-      stop15 = true;
-    }
-
-    if (result != "" && JSON.parse(localStorage.iscust) && result[0].includes(" " + localStorage.custmin + " min") && !stopcust) {
-      new Notification(`Reminder`, {
-        icon: '48.png',
-        body: `${result[0]}`
-      });
-      stopcust = true;
-      stopNow = false;
-
-    }
-  });
-
-
-
+  );
 }
-
 
 function show() {
-  chrome.tabs.query({
-    url: ["https://outlook.office365.com/mail/*", "https://outlook.office.com/mail/*"]
-  }, function (tabs) {
-    var current = tabs[0];
-    if (current == null) {
-      return;
-    }
-    title = current.url;
-
-    chrome.storage.local.get('activatedFolderIds', function (result) {
-      folders = [];
-      if (result.activatedFolderIds.length > 0) {
-        folders = [...result.activatedFolderIds];
-        folders.map(a => getUnreadCount(current, a));
+  chrome.tabs.query(
+    {
+      url: [
+        "https://outlook.office365.com/mail/*",
+        "https://outlook.office.com/mail/*",
+      ],
+    },
+    function (tabs) {
+      var current = tabs[0];
+      if (current == null) {
+        return;
       }
-    });
+      title = current.url;
 
-    getUpComingMeeting(current);
+      chrome.storage.local.get("activatedFolderIds", function (result) {
+        folders = [];
+        if (result.activatedFolderIds.length > 0) {
+          folders = [...result.activatedFolderIds];
+          folders.map((a) => getUnreadCount(current, a));
+        }
+      });
 
-  });
-
+      getUpComingMeeting(current);
+    }
+  );
 }
-
 
 // Conditionally initialize the options.
 if (!localStorage.isInitialized) {
@@ -136,7 +161,6 @@ if (window.Notification) {
   // show();
   // }
 
-
   show();
 
   //var interval = 0; // The display interval, in minutes.
@@ -144,9 +168,7 @@ if (window.Notification) {
   setInterval(function () {
     //sinterval++;
 
-    if (
-      JSON.parse(localStorage.isActivated)
-    ) {
+    if (JSON.parse(localStorage.isActivated)) {
       show();
       interval = 0;
     }
--- notifications/options.js
+++ notifications/options.js
@@ -1,5 +1,5 @@
 function ghost(isDeactivated) {
-  options.style.color = isDeactivated ? 'graytext' : 'black';
+  options.style.color = isDeactivated ? "graytext" : "black";
   // The label color.
   let folderDiv = document.getElementById("folderDiv");
   folderDiv.disabled = isDeactivated; // The control manipulability.
@@ -10,10 +10,9 @@ function ghost(isDeactivated) {
   for (let folderName of folderNames) {
     folderName.disabled = true;
   }
-
 }
 
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   // Initialize the option controls.
   options.isActivated.checked = JSON.parse(localStorage.isActivated);
   // The display activation.
@@ -28,14 +27,13 @@ window.addEventListener('load', function () {
     ghost(!options.isActivated.checked);
   };
 
-
-
   getAllFolder();
 });
 
-
 // document.getElementById('refersh').addEventListener('click', getAllFolder);
-document.getElementById('save').addEventListener('click', saveActivatedFolderList);
+document
+  .getElementById("save")
+  .addEventListener("click", saveActivatedFolderList);
 
 function getAllFolder() {
   let min15 = document.getElementById("min15");
@@ -56,79 +54,90 @@ function getAllFolder() {
     iscust.checked = true;
   }
 
- custmin.value = JSON.parse(localStorage.custmin);
- 
-  chrome.tabs.query({
-    url: ["https://outlook.office365.com/mail/*", "https://outlook.office.com/mail/*"],
-  }, function (tabs) {
-    var current = tabs[0];
-    if (!current) {
-      return;
-    }
-    chrome.tabs.executeScript(current.id, {
-      code: `var x= document.querySelectorAll(\"div[role=treeitem]\");
+  custmin.value = JSON.parse(localStorage.custmin);
+
+  chrome.tabs.query(
+    {
+      url: [
+        "https://outlook.office365.com/mail/*",
+        "https://outlook.office.com/mail/*",
+      ],
+    },
+    function (tabs) {
+      var current = tabs[0];
+      if (!current) {
+        return;
+      }
+      chrome.tabs.executeScript(
+        current.id,
+        {
+          code: `var x= document.querySelectorAll(\"div[role=treeitem] > span:first-of-type\");
       var arr=[];
       for (var item of x){
-    	 if(item.getAttribute("aria-level") != 1) {
-    	  arr.push(item.getAttribute("title"));
-    	  }
+
+    	  arr.push(item.innerText);
+    	  
       }
       arr
-      `
-    }, function (result) {
-      var originalFolderNames = result[0];
-      var folderNames = [...new Set(originalFolderNames)];
-      var list = document.getElementById("folderList");
-      let activatedFolders = [];
-      while (list.firstChild) {
-        list.removeChild(list.firstChild);
-      }
-      //get list of already activated folders
-      chrome.storage.local.get('activatedFolderIds', function (result) {
-        console.log(result.activatedFolderIds);
-        activatedFolders = result.activatedFolderIds;
-
-        folderNames.map(a => {
-          let l = document.createElement("li");
-          let input = document.createElement("input");
-          input.type = "checkbox";
-          input.name = a;
-          if (activatedFolders && activatedFolders.includes(a)) {
-            input.checked = true;
+      `,
+        },
+        function (result) {
+          var originalFolderNames = result[0];
+          var folderNames = [...new Set(originalFolderNames)];
+          var list = document.getElementById("folderList");
+          let activatedFolders = [];
+          while (list.firstChild) {
+            list.removeChild(list.firstChild);
           }
-          input.setAttribute("class", "folderNames");
-          l.appendChild(input);
-          l.appendChild(document.createTextNode(a));
-          list.appendChild(l);
-        });
+          //get list of already activated folders
+          chrome.storage.local.get("activatedFolderIds", function (result) {
+            console.log(result.activatedFolderIds);
+            activatedFolders = result.activatedFolderIds;
 
-      });
+            folderNames.map((a) => {
+              let l = document.createElement("li");
+              let input = document.createElement("input");
+              input.type = "checkbox";
+              input.name = a;
+              if (activatedFolders && activatedFolders.includes(a)) {
+                input.checked = true;
+              }
+              input.setAttribute("class", "folderNames");
+              l.appendChild(input);
+              l.appendChild(document.createTextNode(a));
+              list.appendChild(l);
+            });
+          });
 
-
-      chrome.storage.local.get({
-        'folderNames': []
-      }, function (result) {
-        folderNames.push({
-          folderId: result.folderNames
-        });
-        chrome.storage.local.set({
-          folderIds: folderNames
-        }, function () {
-          // you can use strings instead of objects
-          // if you don't want to define default values
-          //          chrome.storage.local.get('folderIds', function (result) {
-          //            console.log(result.folderIds);
-          //          });
-        });
-      });
-
-    });
-  });
-
+          chrome.storage.local.get(
+            {
+              folderNames: [],
+            },
+            function (result) {
+              folderNames.push({
+                folderId: result.folderNames,
+              });
+              chrome.storage.local.set(
+                {
+                  folderIds: folderNames,
+                },
+                function () {
+                  // you can use strings instead of objects
+                  // if you don't want to define default values
+                  //          chrome.storage.local.get('folderIds', function (result) {
+                  //            console.log(result.folderIds);
+                  //          });
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
 }
 
 function saveActivatedFolderList() {
-
   let min15 = document.getElementById("min15");
   localStorage.min15 = min15.checked;
   let min10 = document.getElementById("min10");
@@ -138,39 +147,52 @@ function saveActivatedFolderList() {
   let iscust = document.getElementById("iscust");
   localStorage.iscust = iscust.checked;
   let custmin = null;
-  
-    custmin = document.getElementById("custmin");
-    localStorage.custmin = custmin.value;
-  
-  chrome.tabs.query({
-    url: ["https://outlook.office365.com/mail/*", "https://outlook.office.com/mail/*"]
-  }, function (tabs) {
 
-    var current = tabs[0];
-    if (!current) {
-      return;
-    }
+  custmin = document.getElementById("custmin");
+  localStorage.custmin = custmin.value;
 
-    var x = document.querySelectorAll("input.folderNames[type=checkbox]:checked");
-    var activatedFolderNames = [];
-    for (var item of x) {
-      activatedFolderNames.push(item.getAttribute("name"));
+  chrome.tabs.query(
+    {
+      url: [
+        "https://outlook.office365.com/mail/*",
+        "https://outlook.office.com/mail/*",
+      ],
+    },
+    function (tabs) {
+      var current = tabs[0];
+      if (!current) {
+        return;
+      }
+
+      var x = document.querySelectorAll(
+        "input.folderNames[type=checkbox]:checked"
+      );
+      var activatedFolderNames = [];
+      for (var item of x) {
+        activatedFolderNames.push(item.getAttribute("name"));
+      }
+      chrome.storage.local.get(
+        {
+          activatedFolderNames: [],
+        },
+        function (result) {
+          //        	  activatedFolderNames.push({
+          //                  activatedFolderIds: result.activatedFolderIds
+          //                });
+          chrome.storage.local.set(
+            {
+              activatedFolderIds: activatedFolderNames,
+            },
+            function () {
+              // you can use strings instead of objects
+              // if you don't want to define default values
+              chrome.storage.local.get("activatedFolderIds", function (result) {
+                console.log(result.activatedFolderIds);
+              });
+            }
+          );
+        }
+      );
     }
-    chrome.storage.local.get({
-      'activatedFolderNames': []
-    }, function (result) {
-      //        	  activatedFolderNames.push({
-      //                  activatedFolderIds: result.activatedFolderIds
-      //                });
-      chrome.storage.local.set({
-        activatedFolderIds: activatedFolderNames
-      }, function () {
-        // you can use strings instead of objects
-        // if you don't want to define default values
-        chrome.storage.local.get('activatedFolderIds', function (result) {
-          console.log(result.activatedFolderIds);
-        });
-      });
-    });
-  });
+  );
 }
